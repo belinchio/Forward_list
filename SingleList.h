8bd8979adc0d288279ef6b8a1982e3ad5f906786
belinchio <belinchio@yandex.ru>
2024-04-14 17:56:04 +0300

    refactored SingleList.h

--- SingleList.h
+++ SingleList.h
@@ -16,8 +16,8 @@ public:
 
 private:
     struct node {
-        node *next = nullptr;
-        T data;
+        node* next = nullptr;
+        value_type data;
 
         node(value_type item) noexcept : data {std::move(item)} {};
     };
@@ -30,11 +30,11 @@ public:
         explicit ForwardListConstIterator(const node* ptr) noexcept : m_current {ptr} {}
 
     public:
-        using   difference_type   = Forward_list::difference_type;
-        using   value_type        = Forward_list::value_type;
-        using   pointer           = Forward_list::const_pointer;
-        using   reference         = Forward_list::const_reference;
-        using   iterator_category = std::forward_iterator_tag;
+        using difference_type   = Forward_list::difference_type;
+        using value_type        = Forward_list::value_type;
+        using pointer           = Forward_list::const_pointer;
+        using reference         = Forward_list::const_reference;
+        using iterator_category = std::forward_iterator_tag;
 
         reference operator*() const noexcept {
             assert(m_current != nullptr);
@@ -55,7 +55,7 @@ public:
             return copy;
         }
 
-        bool operator== (ForwardListConstIterator other) const noexcept {
+        bool operator==(ForwardListConstIterator other) const noexcept {
             return m_current == other.m_current;
         }
 
@@ -79,14 +79,14 @@ public:
         explicit ForwardListIterator(node* ptr) noexcept : ForwardListConstIterator {ptr} {}
 
     public:
-        using   difference_type     = Forward_list::difference_type;
-        using   value_type          = Forward_list::value_type;
-        using   pointer             = Forward_list::pointer;
-        using   reference           = Forward_list::reference;
-        using   iterator_category   = std::forward_iterator_tag;
+        using difference_type     = Forward_list::difference_type;
+        using value_type          = Forward_list::value_type;
+        using pointer             = Forward_list::pointer;
+        using reference           = Forward_list::reference;
+        using iterator_category   = std::forward_iterator_tag;
 
-        reference operator *() noexcept {
-            auto &&res = ForwardListConstIterator::operator*();
+        reference operator*() noexcept {
+            auto&& res = ForwardListConstIterator::operator*();
             return const_cast<reference>(res);
         }
 
@@ -97,7 +97,7 @@ public:
 
         ForwardListIterator operator++(int) noexcept {
             auto res = ForwardListConstIterator::operator++(0);
-            return ForwardListIterator { const_cast<node*>(res.Get()) };
+            return ForwardListIterator {const_cast<node*>(res.Get())};
         }
     };
 
@@ -107,7 +107,7 @@ public:
     Forward_list() = default;
 
     Forward_list(std::initializer_list<T> items) {
-        for (auto &item : items) {
+        for (auto& item : items) {
             push_back(item);
         }
     }
@@ -125,7 +125,7 @@ public:
     }
 
     void push_front(value_type item) {
-        auto newnode = new node { std::move(item) };
+        auto newnode = new node{std::move(item)};
         if (m_head) {
             newnode->next = m_head;
             m_head = newnode;
@@ -145,13 +145,13 @@ public:
     }
 
     void insert_after(const_iterator place, value_type item) {
-        auto ptr = const_cast<node *>(place.Get());
+        auto ptr = const_cast<node*>(place.Get());
         if (!ptr) {
             push_front(std::move(item));
             return;
         }
 
-        auto newnode = new node {std::move(item)};
+        auto newnode = new node{std::move(item)};
         newnode->next = ptr->next;
         ptr->next = newnode;
 }
@@ -162,16 +162,16 @@ public:
                 return it;
             }
         }
-        return const_iterator {nullptr};
+        return const_iterator{nullptr};
     }
 
     iterator find(const_reference item) noexcept {
         auto it = static_cast<const Forward_list &>(*this).find(item);
-        return iterator {const_cast<node *>(it.Get())};
+        return iterator {const_cast<node*>(it.Get())};
     }
 
     void erase_after(const_iterator place) noexcept {
-        auto ptr = const_cast<node *>(place.Get());
+        auto ptr = const_cast<node*>(place.Get());
         if (ptr) {
             auto nodeToDelete = ptr->next;
             if (ptr->next) {
@@ -180,39 +180,39 @@ public:
         delete nodeToDelete;
         } else {
             assert(m_head != nullptr);
-            delete std::exchange (m_head, m_head->next);
+            delete std::exchange(m_head, m_head->next);
         }
     }
 
     void pop_front() noexcept {
-        erase_after(iterator {nullptr});
+        erase_after(iterator{nullptr});
     }
 
     const_iterator begin() const noexcept {
-        return const_iterator {m_head};
+        return const_iterator{m_head};
     }
 
     const_iterator end() const noexcept {
-        return const_iterator {nullptr};
+        return const_iterator{nullptr};
     }
 
     const_iterator cbegin() const noexcept {
-        return const_iterator {m_head};
+        return const_iterator{m_head};
     }
 
     const_iterator cend() const noexcept {
-        return const_iterator {nullptr};
+        return const_iterator{nullptr};
     }
 
     iterator begin() noexcept {
-        return iterator {m_head};
+        return iterator{m_head};
     }
 
     iterator end() noexcept {
-        return iterator {nullptr};
+        return iterator{nullptr};
     }
 
 private:
-    node *m_head = nullptr;
-    node *m_tail = nullptr;
+    node* m_head = nullptr;
+    node* m_tail = nullptr;
 };
